65ac65c841cc8d173363cc7e02962efa1dfb0445
unrealwork <unrealwork@gmail.com>
2018-06-05 11:09:32 +0300

    wrong path

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -166,7 +166,7 @@ module.exports = {
     title: 'Axibase Collector',
     description: "User manual and API reference for Axibase® Time Series Database",
     head: [
-        ['link', {rel: 'shortcut icon', href: '/images/favicon-96x96.png'}]
+        ['link', {rel: 'shortcut icon', href: '/favicon-96x96.png'}]
     ],
     staticFilesExtensionsTest: /(?:tcollector|\.(?:pdf|xlsx?|xml|txt|csv|str|java|json|sql|sps|yxmd|htm|prpt|do|tdc|jsonld|ktr|service))$/,
     themeConfig: {
